34fa9be5defed73f3b2b40147ba0a812e2110744
ElectriPixie <ElectriPixie@gmail.com>
2025-01-26 18:25:12 -0500

    updated modules variables and how they are passed

--- localstack_network/main.tf
+++ localstack_network/main.tf
@@ -1,6 +1,6 @@
 # File: localstack_network.tf
 # Define the LocalStack network
 resource "docker_network" "localstack" {
-  name = var.name
-  driver = var.driver
+  name = var.network_name
+  driver = var.network_driver
 }
--- localstack_network/variables.tf
+++ localstack_network/variables.tf
@@ -1,9 +1,9 @@
-variable "name" {
+variable "network_name" {
   description = "the name of the docker network to create"
   type = string
   default = "localstack"
 }
-variable "driver" {
+variable "network_driver" {
   description = "the name of the docker driver"
   type = string
   default = "bridge"
