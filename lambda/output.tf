8eabbe9ece4dafa5e5cb139ac8df25e7da5b6ee7
ElectriPixie <ElectriPixie@gmail.com>
2025-01-30 09:37:54 -0500

    updated outputs

--- lambda/output.tf
+++ lambda/output.tf
@@ -1,11 +1,3 @@
-output "api_id" {
-  value = aws_api_gateway_rest_api.rest_api.id
-}
-
-output "root_resource_id" {
-  value = aws_api_gateway_rest_api.resource.root_resource_id
-}
-
 output "lambda_invoke_arn" {
   value = aws_lambda_function.lambda.invoke_arn
 }
