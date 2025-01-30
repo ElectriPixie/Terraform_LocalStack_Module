9b6bad5f2886638ac3dcc5ed275c68a96befdfba
ElectriPixie <ElectriPixie@gmail.com>
2025-01-30 17:13:48 -0500

    changed integration_type

--- apigateway/main.tf
+++ apigateway/main.tf
@@ -62,7 +62,7 @@ resource "aws_api_gateway_integration" "integration" {
   resource_id             = aws_api_gateway_resource.resource.id
   http_method             = aws_api_gateway_method.method.http_method
   integration_http_method = "POST"
-  type                    = "AWS_PROXY"
+  type                    = "AWS"
   uri                     = "arn:aws:apigateway:us-east-1:lambda:path/2015-03-31/functions/arn:aws:lambda:us-east-1:${data.aws_caller_identity.current.account_id}:function:${var.lambda_function}/invocations"
 }
 
