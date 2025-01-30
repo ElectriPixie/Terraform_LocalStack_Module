66b45a0e641e41617b224778f60bac14418a8a04
ElectriPixie <ElectriPixie@gmail.com>
2025-01-30 09:53:37 -0500

    updated network so it won't create a new one if one exists

--- localstack_network/main.tf
+++ localstack_network/main.tf
@@ -1,6 +1,11 @@
 # File: localstack_network.tf
 # Define the LocalStack network
-resource "docker_network" "localstack" {
+data "docker_network" "localstack" {
   name = var.network_name
+}
+
+resource "docker_network" "localstack" {
+  name   = var.network_name
   driver = var.network_driver
+  count  = data.docker_network.localstack.id == "" ? 1 : 0
 }
