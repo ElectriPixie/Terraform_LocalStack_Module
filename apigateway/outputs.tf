b79a273d79183ea6ada04e664f59ee209fb1aa40
ElectriPixie <ElectriPixie@gmail.com>
2025-01-30 16:10:37 -0500

    updated module

--- apigateway/outputs.tf
+++ apigateway/outputs.tf
@@ -1,8 +1,8 @@
-#output "api_id" {
-#  value = aws_api_gateway_rest_api.rest_api.id
-#}
+output "api_id" {
+  value = aws_api_gateway_rest_api.rest_api.id
+}
 
-#output "root_resource_id" {
-#  value = aws_api_gateway_rest_api.rest_api.root_resource_id
-#}
+output "root_resource_id" {
+  value = aws_api_gateway_rest_api.rest_api.root_resource_id
+}
 
