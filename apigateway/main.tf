e141fc5c137493f376ac4d2352f1513ae5eefd87
ElectriPixie <ElectriPixie@gmail.com>
2025-01-30 13:34:07 -0500

    changed module

--- apigateway/main.tf
+++ apigateway/main.tf
@@ -3,9 +3,6 @@ resource "aws_api_gateway_rest_api" "rest_api" {
   # The name of the REST API.
   name        = "rest_api"
   description = "A REST API"
-  #tags = {
-  #  "_custom_id_" = "rest_api"
-  #}
 }
 
 resource "aws_api_gateway_resource" "resource" {
@@ -58,7 +55,7 @@ resource "aws_api_gateway_integration" "integration" {
   resource_id             = aws_api_gateway_resource.resource.id
   http_method             = aws_api_gateway_method.method.http_method
   integration_http_method = aws_api_gateway_method.method.http_method
-  type                    = "AWS"
+  type                    = "MOCK"
   uri                     = "arn:aws:apigateway:us-east-1:lambda:path/2015-03-31/functions/arn:aws:lambda:us-east-1:${data.aws_caller_identity.current.account_id}:function:${var.lambda_function}/invocations"
 }
 
