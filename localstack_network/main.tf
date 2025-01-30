fdb9822175ae1f6900b9f47f46ffd5764169419a
ElectriPixie <ElectriPixie@gmail.com>
2025-01-30 10:02:31 -0500

    updated network creation

--- localstack_network/main.tf
+++ localstack_network/main.tf
@@ -1,12 +1,11 @@
 # File: localstack_network.tf
 # Define the LocalStack network
-data "docker_network" "localstack" {
+data "docker_network" "existing_network" {
   name = var.network_name
 }
 
 resource "docker_network" "localstack" {
-  name       = var.network_name
-  driver     = var.network_driver
-  count      = data.docker_network.localstack.id == "" ? 1 : 0
-  depends_on = [data.docker_network.localstack]
+  name   = var.network_name
+  driver = var.network_driver
+  id     = try(data.docker_network.existing_network.id, data.docker_network.existing_network.name)
 }
