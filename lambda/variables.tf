515ede021e5339145fc8bd3c6b52354977d61962
ElectriPixie <ElectriPixie@gmail.com>
2025-01-30 19:02:38 -0500

    pruned variables

--- lambda/variables.tf
+++ lambda/variables.tf
@@ -27,15 +27,3 @@ variable "role_name" {
   default     = "lambda_exec"
   description = "The name of the IAM role"
 }
-
-variable "api_path" {
-  type        = string
-  default     = "lambda"
-  description = "The path of the API"
-}
-
-variable "http_method" {
-  type        = string
-  default     = "GET"
-  description = "The HTTP method for the API"
-}
