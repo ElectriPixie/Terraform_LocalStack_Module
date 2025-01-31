a9762ef49edde9a83254e0f7c5f5516f446a0b03
ElectriPixie <ElectriPixie@gmail.com>
2025-01-30 19:08:13 -0500

    update module

--- apigateway/main.tf
+++ apigateway/main.tf
@@ -63,7 +63,7 @@ resource "aws_api_gateway_integration" "integration" {
   http_method             = aws_api_gateway_method.method.http_method
   integration_http_method = "POST"
   type                    = "AWS"
-  uri                     = "arn:aws:apigateway:us-east-1:lambda:path/2015-03-31/functions/arn:aws:lambda:us-east-1:${data.aws_caller_identity.current.account_id}:function:lambda/invocations"
+  uri                     = "arn:aws:apigateway:us-east-1:lambda:path/2015-03-31/functions/arn:aws:lambda:us-east-1:${data.aws_caller_identity.current.account_id}:function:${var.lambda_function}/invocations"
 }
 
 resource "aws_api_gateway_deployment" "deployment" {
@@ -83,4 +83,12 @@ resource "aws_lambda_permission" "apigw" {
   function_name = var.lambda_function
   principal     = "apigateway.amazonaws.com"
   source_arn    = "${aws_api_gateway_rest_api.rest_api.execution_arn}/*/*/*"
+}
+
+resource "aws_api_gateway_integration_response" "integration_response" {
+  depends_on  = [aws_api_gateway_integration.integration]
+  rest_api_id = aws_api_gateway_rest_api.rest_api.id
+  resource_id = aws_api_gateway_resource.resource.id
+  http_method = aws_api_gateway_method.method.http_method
+  status_code = "200"
 }
